a2f3e19187b6995a8cd1d383cd44072d87042b03
George Toothman <george.toothman@gmail.com>
2015-08-11 21:24:06 -0400

    change array to hash with index values

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,17 @@
+require 'pry'
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |animal|
+    if new_hash.keys.include?(animal)
+      new_hash[animal] += 1
+    else
+      new_hash[animal] = 1
+    end
+  end
+  new_hash
 end
+
+# use array.count(animal) to produce the value
+# of new_hash[animal]
+
  
